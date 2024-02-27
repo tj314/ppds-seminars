ba1f7083eb03b8702017da6d08f1a26e507d897c
tj314 <tj314@tutanota.com>
2024-02-27 20:46:37 +0100

    feat: implement a template for lightswitch exercise

---
+++ seminar3/lightswitch.py
@@ -0,0 +1,20 @@
+"""This module implements the LightSwitch data type."""
+
+__author__ = "Tomáš Vavro"
+__email__ = "tomas.vavro@stuba.sk"
+__license__ = "MIT"
+
+
+class LightSwitch:
+    """This class implements the LightSwitch data type."""
+    def __init__(self):
+        # TODO: Implement the initialization of the LightSwitch data type
+        pass
+
+    def lock(self, semaphore):
+        # TODO: Implement the locking mechanism
+        pass
+
+    def unlock(self, semaphore):
+        # TODO: Implement the unlocking mechanism
+        pass
---
+++ seminar3/readers_writers.py
@@ -0,0 +1,56 @@
+"""This module implements a solution to the readers writers problem using the LightSwitch data type."""
+
+__author__ = "Tomáš Vavro"
+__email__ = "tomas.vavro@stuba.sk"
+__license__ = "MIT"
+
+
+from lightswitch import LightSwitch
+from fei.ppds import Thread, Mutex, print
+from time import sleep
+
+
+class Shared:
+    """This class contains the shared data and the synchronization objects."""
+    def __init__(self):
+        self.ls = LightSwitch()
+        self.lock = Mutex()
+
+
+def reader(shared, tid):
+    """This function represents the reader's behavior.
+
+    :param shared: an instance of the Shared class
+    :param tid: thread id
+    """
+    while True:
+        shared.ls.lock(shared.lock)
+        print(f"Reader {tid} is reading.")
+        sleep(0.1)
+        shared.ls.unlock(shared.lock)
+
+
+def writer(shared, tid):
+    """This function represents the writer's behavior.
+
+    :param shared: an instance of the Shared class
+    :param tid: thread id
+    """
+    while True:
+        shared.lock.lock()
+        print(f"Writer {tid} is writing.")
+        sleep(0.2)
+        shared.lock.unlock()
+
+
+def main():
+    num_readers = 5
+    num_writers = 10
+    shared = Shared()
+    threads = [Thread(reader, shared, i) for i in range(num_readers)] +\
+              [Thread(writer, shared, i) for i in range(num_writers)]
+    [t.join() for t in threads]
+
+
+if __name__ == "__main__":
+    main()
