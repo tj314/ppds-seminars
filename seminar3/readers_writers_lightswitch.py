498e54ebe8a0e0215b3c84237ba716c254439c0a
tj314 <tj314@tutanota.com>
2024-02-27 21:12:58 +0100

    feat: add a turnstile to ensure priority of the writer threads over the reader threads

--- seminar3/readers_writers_lightswitch.py
+++ seminar3/readers_writers_lightswitch.py
@@ -6,7 +6,7 @@ __license__ = "MIT"
 
 
 from lightswitch import LightSwitch
-from fei.ppds import Thread, Mutex, print
+from fei.ppds import Thread, Semaphore, print
 from time import sleep
 
 
@@ -14,7 +14,8 @@ class Shared:
     """This class contains the shared data and the synchronization objects."""
     def __init__(self):
         self.ls = LightSwitch()
-        self.lock = Mutex()
+        self.lock = Semaphore(1)
+        self.writers_priority = Semaphore(1)
 
 
 def reader(shared, tid):
@@ -24,10 +25,17 @@ def reader(shared, tid):
     :param tid: thread id
     """
     while True:
+        # turnstile to ensure readers priority
+        shared.writers_priority.wait()
+        shared.writers_priority.signal()
+
+        # reading
         shared.ls.lock(shared.lock)
         print(f"Reader {tid} is reading.")
         sleep(0.1)
         shared.ls.unlock(shared.lock)
+        print(f"Reader {tid} finished reading.")
+        sleep(0.2)  # simulate the time between two consecutive readings
 
 
 def writer(shared, tid):
@@ -37,10 +45,14 @@ def writer(shared, tid):
     :param tid: thread id
     """
     while True:
-        shared.lock.lock()
+        shared.writers_priority.wait()
+        shared.lock.wait()
         print(f"Writer {tid} is writing.")
         sleep(0.2)
-        shared.lock.unlock()
+        shared.lock.signal()
+        shared.writers_priority.signal()
+        print(f"Writer {tid} finished writing.")
+        sleep(0.5)  # simulate the time between two consecutive writings
 
 
 def main():
