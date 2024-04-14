e16ecf0287ae342c5cde8389ac3b31127457f692
tj314 <tj314@tutanota.com>
2024-04-15 01:16:42 +0200

    feat: implement some simple MPI exercises

---
+++ seminar9/main_v1.py
@@ -0,0 +1,64 @@
+"""This module implements a scalar product of two vectors using MPI.
+
+The two vectors are randomly generated and then broadcast to all processors.
+This solution is not optimal, as the data is broadcasted to all processors,
+even though only a part of it is needed.
+"""
+
+__author__ = "Tomáš Vavro, Marián Šebeňa"
+__licence__ = "MIT"
+
+from mpi4py import MPI
+import numpy as np
+
+N = 101  # number of elements in the vectors
+
+
+def main():
+    world = MPI.COMM_WORLD
+    world_size = world.Get_size()
+    rank = world.Get_rank()
+
+    per_processor_range = N // world_size
+
+    # initialize the vectors and calculate workflows
+    if rank == 0:
+        a = np.random.rand(N)
+        b = np.random.rand(N)
+        # calculate the workload for each processor
+        processor_ranges = np.array([per_processor_range for _ in range(world_size)], dtype=np.int32)
+        for i in range(N % world_size):
+            processor_ranges[i] += 1
+        # calculate the expected dot product in order to verify the result
+        expected_dot_product = np.dot(a, b)
+    else:
+        a = np.empty(N, dtype=np.float64)
+        b = np.empty(N, dtype=np.float64)
+        processor_ranges = np.empty(world_size, dtype=np.int32)
+        expected_dot_product = None
+
+    # broadcast the data two vectors and the workloads
+    world.Bcast([a, MPI.DOUBLE], root=0)
+    world.Bcast([b, MPI.DOUBLE], root=0)
+    world.Bcast([processor_ranges, MPI.INT], root=0)
+
+    # calculate the partial dot product
+    start = np.sum(processor_ranges[:rank])
+    result = 0
+    for i in range(start, start + processor_ranges[rank]):
+        result += a[i] * b[i]
+    
+    # is the barrier necessary?
+    world.Barrier()
+
+    # reduce the results
+    total_result = np.zeros(1, dtype=np.float64)
+    world.Reduce(result, total_result, op=MPI.SUM)
+
+    if rank == 0:
+        print(f"Computed dot product: {total_result[0]}")
+        print(f"Expected dot product: {expected_dot_product}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ seminar9/main_v4.py
@@ -0,0 +1,68 @@
+"""This module implements a scalar product of two vectors using MPI.
+
+The two vectors are randomly generated and then scattered between processors.
+The final result is calculated by employing parallel reduction.
+In addition, the partial results are printed in order of ranks.
+In order to achieve this, a token ring is used.
+"""
+
+__author__ = "Tomáš Vavro, Marián Šebeňa"
+__licence__ = "MIT"
+
+from mpi4py import MPI
+import numpy as np
+
+N = 10  # number of elements in the vectors
+
+
+def main():
+    world = MPI.COMM_WORLD
+    world_size = world.Get_size()
+    rank = world.Get_rank()
+
+    per_processor_range = N // world_size
+
+    # initialize the vectors
+    a = None
+    b = None
+    if rank == 0:
+        a = np.random.rand(N)
+        b = np.random.rand(N)
+        # pad the arrays to make them divisible by the number of processors
+        if N % world_size != 0:
+            a = np.pad(a, (0, world_size - N % world_size), mode="constant")
+            b = np.pad(b, (0, world_size - N % world_size), mode="constant")
+    
+    # scatter the data
+    local_a = np.empty(per_processor_range + (N % world_size != 0), dtype="d")
+    local_b = np.empty(per_processor_range + (N % world_size != 0), dtype="d")
+    world.Scatter(a, recvbuf=local_a, root=0)
+    world.Scatter(b, recvbuf=local_b, root=0)
+    
+    # calculate the partial dot product
+    result = 0
+    for i in range(len(local_a)):
+        result += local_a[i] * local_b[i]
+
+    # print the partial results in order of ranks
+    # the master processor sends a token to the next processor
+    # the last processor sends a token back to the master processor
+    if rank == 0:
+        print(f"Rank {rank} computed dot product: {result}")
+        world.send(1, dest=1)
+        _ = world.recv(source=world_size - 1)
+    else:
+        _ = world.recv(source=rank - 1)
+        print(f"Rank {rank} computed dot product: {result}")
+        world.send(1, dest=(rank + 1) % world_size)
+    
+    # reduce the results
+    total_result = np.zeros(1, dtype=np.float64)
+    world.Reduce([result, MPI.DOUBLE], [total_result, MPI.DOUBLE], op=MPI.SUM)
+    if rank == 0:
+        print(f"Computed dot product: {total_result[0]}")
+        print(f"Expected dot product: {np.dot(a, b)}")
+
+
+if __name__ == "__main__":
+    main()
