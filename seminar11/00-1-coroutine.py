f7c132e0c34ca54947fbe47efa58034a2e33634b
Roderik Ploszek <roderik.ploszek@gmail.com>
2024-04-24 22:30:31 +0200

    feat: add examples from the seminar that were created later

---
+++ seminar11/00-1-coroutine.py
@@ -0,0 +1,27 @@
+from collections.abc import Generator
+
+
+def complain_about(substring: str):
+    print('Please talk to me!')
+    try:
+        while True:
+            text = yield
+            if substring in text:
+                print(f'Oh no: I found a {substring} again!')
+    except GeneratorExit:
+        print('Ok, ok: I am quitting.')
+    except RuntimeError:
+        print('Something funny happened.')
+
+
+it = complain_about('Ruby')
+
+next(it)
+it.send('ahoj Ruby')
+it.throw(RuntimeError)
+# it.send('ahoj svjete!')
+# it.send('no znovu Ruby na riadku')
+# it.close()
+
+# it.send('toto nepojde')
+# next(it)
